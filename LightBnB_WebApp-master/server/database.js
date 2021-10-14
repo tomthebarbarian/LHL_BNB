2de9728981ea975d79c9a432e145274a03e4d659
Tom Zhang <tom.zhang989@gmail.com>
2021-10-14 18:05:32 -0400

    created a pool closure

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -2,10 +2,9 @@ const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
 const { Pool } = require('pg');
-const {argv} = require('process');
 
-const rawArgs = argv.slice(2);
 
+// Pool Closure for other function
 const pool = new Pool({
   user: 'tomzhang',
   password: '',
@@ -13,7 +12,11 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-
+const helpPool = {
+  query : (text, params) => {
+    return pool.query(text, params);
+  }
+};
 
 /// Users
 
@@ -23,19 +26,14 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  // console.log('in email', `'${email}'`);
-  return pool.query(
-    `
-    SELECT *
-    FROM users
-    WHERE users.email = $1;
-    `
-    , [`${email}`]
+  const queryStr = `
+  SELECT *
+  FROM users
+  WHERE users.email = $1;
+  `;
+  return helpPool.query(queryStr, [`${email}`]
   ).then(
     (result) => {
-      // console.log(result);
-      // console.log('returning',result.rows);
-      // console.log('returning',result.rows[0].password);
       return result.rows[0];
     }
   ).catch(
@@ -57,7 +55,7 @@ exports.getUserWithEmail = getUserWithEmail;
 // };
 
 const getUserWithId = function(id) {
-  return pool.query(
+  return helpPool.query(
     `
     SELECT *
     FROM users
@@ -82,16 +80,8 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-
-// const addUser =  function(user) {
-//   const userId = Object.keys(users).length + 1;
-//   user.id = userId;
-//   users[userId] = user;
-//   return Promise.resolve(user);
-// };
-
 const addUser =  function(user) {
-  return pool.query(
+  return helpPool.query(
     `
     INSERT INTO users (name, email, password)
     VALUES ($1,$2,$3)
@@ -109,14 +99,8 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-
-// const getAllReservations = function(guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// };
-
-
 const getAllReservations = function(guest_id, limit = 10) {
-  return pool
+  return helpPool
     .query(
       `SELECT *
       FROM reservations
@@ -134,21 +118,12 @@ const getAllReservations = function(guest_id, limit = 10) {
 exports.getAllReservations = getAllReservations;
 
 /// Properties
-
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// };
 const getAllProperties = (options, limit = 10) => {
   // 1
   const queryParams = [];
@@ -204,7 +179,7 @@ const getAllProperties = (options, limit = 10) => {
   // 5
   console.log(queryString, queryParams);
   
-  return pool
+  return helpPool
     .query(queryString, queryParams)
     .then((result) => result.rows)
     .catch((err) => {
@@ -221,14 +196,6 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function(property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// };
-
-
 const addProperty = function(property) {
   const {owner_id , title , description , thumbnail_photo_url , cover_photo_url , 
     cost_per_night,parking_spaces, number_of_bathrooms, number_of_bedrooms,
@@ -246,7 +213,7 @@ const addProperty = function(property) {
     VALUES ($1, $2 ,$3, $4, $5, $6, $7, $8, $9, $10, $11 , $12, $13 , $14)
     RETURNING *;
     `;
-  return pool
+  return helpPool
     .query(queryStr, queryParams)
     .then((result) => {
       console.log(result.rows);
